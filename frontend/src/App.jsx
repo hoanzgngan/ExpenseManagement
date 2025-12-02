b33fd76694ea4feba57e53232fd29d065f7f44b1
hoangngan47 <phamhoangngan1210@gmail.com>
2025-12-03 03:38:49 +0700

    fc

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-600 text-white text-3xl font-bold">
+      App chạy OK ✅
+    </div>
+  );
+}
+
+export default App;
